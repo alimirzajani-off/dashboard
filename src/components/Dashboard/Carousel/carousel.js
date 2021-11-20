c12ce5aef2125e4bfd847a6864077e44ac304f57
ali <alimahdimirzajani.am@gmail.com>
2021-11-20 11:49:57 +0330

    edit package

--- src/components/Dashboard/Carousel/carousel.js
+++ src/components/Dashboard/Carousel/carousel.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "antd/dist/antd.css";
 import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
-// import { Carousel } from "antd";
 import "./carousel.sass";
 import image1 from "../../../assets/amar.png";
 import image2 from "../../../assets/eghtesad.png";
@@ -9,39 +8,6 @@ import image3 from "../../../assets/tosee.png";
 import image4 from "../../../assets/gomrok.png";
 import image5 from "../../../assets/bazargani.png";
 import image6 from "../../../assets/daroo.png";
-
-// export default class CarouselComponent extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.next = this.next.bind(this);
-//     this.previous = this.previous.bind(this);
-//     this.carousel = React.createRef();
-//   }
-//   next() {
-//     this.carousel.next();
-//   }
-//   previous() {
-//     this.carousel.prev();
-//   }
-
-//   render() {
-//     return (
-//       <div >
-//
-//         <Carousel className="acarousel" ref={(node) => (this.carousel = node)}>
-//           <div>
-//
-//           <div>
-//             <div className="contentStyle">
-//
-//             </div>
-//           </div>
-//         </Carousel>
-//
-//       </div>
-//     );
-//   }
-// }
 
 const Carousel = () => {
   const [ToggelShow, setToggelShow] = useState(true);
